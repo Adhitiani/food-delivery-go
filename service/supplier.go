a76367826ba6b6ec7cb3752dc5b9efb0d49d3a0b
adhitiani.suzanthi <adhitiani@gmail.com>
2024-09-19 18:03:05 +0200

    add concureny for supplier with pool package

--- service/supplier.go
+++ service/supplier.go
@@ -6,10 +6,11 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"project/food-delivery/model"
 	"project/food-delivery/repository"
 	"project/food-delivery/util"
-	"sync"
+	"project/food-delivery/util/pool"
 	"time"
 )
 
@@ -97,77 +98,154 @@ func (s *SupplierService) FetchSuppliers(url string) ([]model.Supplier, error) {
 
 // }
 
-const maxWorkers = 3
+//const maxWorkers = 3
+
+// func (s *SupplierService) FetchAndInsertSuppliers() error {
+// 	start := time.Now()
+// 	defer func() {
+// 		log.Printf("Time taken: %v\n", time.Since(start)) // Log time taken
+// 	}()
+
+// 	limit := 20
+// 	page := 1
+
+// 	var allSuppliers []model.Supplier
+// 	var wg sync.WaitGroup
+// 	supplierCh := make(chan []model.Supplier)
+// 	errCh := make(chan error)
+// 	doneCh := make(chan struct{})
+
+// 	// Launch a goroutine to collect suppliers from the supplierCh
+// 	go func() {
+// 		for suppliers := range supplierCh {
+// 			allSuppliers = append(allSuppliers, suppliers...)
+// 		}
+// 		// Signal that all suppliers have been collected
+// 		doneCh <- struct{}{}
+// 	}()
+
+// 	// Start workers to fetch suppliers concurrently
+// 	for i := 0; i < maxWorkers; i++ {
+// 		wg.Add(1)
+// 		go func(page int) {
+// 			defer wg.Done()
+// 			for {
+// 				url := fmt.Sprintf("https://foodapi.golang.nixdev.co/suppliers?limit=%d&page=%d", limit, page)
+
+// 				suppliers, err := s.FetchSuppliers(url)
+// 				if err != nil {
+// 					errCh <- fmt.Errorf("error fetching supplier at page %d: %v", page, err)
+// 					return
+// 				}
+
+// 				// If there are no more suppliers, break the loop
+// 				if len(suppliers) == 0 {
+// 					break
+// 				}
+
+// 				// Send the fetched suppliers to the channel
+// 				supplierCh <- suppliers
+
+// 				page++
+// 			}
+// 		}(page)
+// 	}
+
+// 	// Wait for all workers to finish
+// 	go func() {
+// 		wg.Wait()
+// 		close(supplierCh)
+// 	}()
+
+// 	// Listen for errors and results
+// 	select {
+// 	case err := <-errCh:
+// 		return err
+// 	case <-doneCh:
+// 		// Insert all fetched suppliers into the database
+// 		err := s.repo.InsertSuppliers(allSuppliers)
+// 		if err != nil {
+// 			return fmt.Errorf("error inserting suppliers into database: %v", err)
+// 		}
+// 		log.Println("Suppliers inserted into the database successfully!")
+// 	}
+
+// 	return nil
+// }
 
 func (s *SupplierService) FetchAndInsertSuppliers() error {
 	start := time.Now()
 	defer func() {
-		log.Printf("Time taken: %v\n", time.Since(start)) // Log time taken
+		log.Printf("Time taken: %v\n", time.Since(start))
 	}()
 
-	limit := 20
-	page := 1
+	const numSuppliers = 104
+	const limit = 20
+	const maxWorkers = 3
+	var totalPages = int(math.Ceil(float64(numSuppliers) / float64(limit)))
 
-	var allSuppliers []model.Supplier
-	var wg sync.WaitGroup
-	supplierCh := make(chan []model.Supplier)
+	suppliersResult := make([]model.Supplier, 0)
+	resCh := make(chan any)
 	errCh := make(chan error)
-	doneCh := make(chan struct{})
 
-	// Launch a goroutine to collect suppliers from the supplierCh
-	go func() {
-		for suppliers := range supplierCh {
-			allSuppliers = append(allSuppliers, suppliers...)
-		}
-		// Signal that all suppliers have been collected
-		doneCh <- struct{}{}
-	}()
+	wPool := pool.NewWorkerPool(resCh, errCh).WithBrokerCount(maxWorkers)
+	wPool.Start()
 
-	// Start workers to fetch suppliers concurrently
-	for i := 0; i < maxWorkers; i++ {
-		wg.Add(1)
-		go func(page int) {
-			defer wg.Done()
-			for {
-				url := fmt.Sprintf("https://foodapi.golang.nixdev.co/suppliers?limit=%d&page=%d", limit, page)
-
-				suppliers, err := s.FetchSuppliers(url)
-				if err != nil {
-					errCh <- fmt.Errorf("error fetching supplier at page %d: %v", page, err)
-					return
-				}
-
-				// If there are no more suppliers, break the loop
-				if len(suppliers) == 0 {
-					break
-				}
-
-				// Send the fetched suppliers to the channel
-				supplierCh <- suppliers
-
-				page++
+	// Append jobs for each page
+	for page := 1; page <= totalPages; page++ {
+		page := page // capture the current page
+		log.Printf("Appending job for page %d", page)
+		wPool.Append(func() (any, error) {
+			url := fmt.Sprintf("https://foodapi.golang.nixdev.co/suppliers?limit=%d&page=%d", limit, page)
+			suppliers, err := s.FetchSuppliers(url)
+			if err != nil {
+				return nil, fmt.Errorf("error fetching page %d: %w", page, err)
 			}
-		}(page)
+			return suppliers, nil // Return the suppliers as `any`
+		})
 	}
 
-	// Wait for all workers to finish
+	// Collect results in a separate goroutine
 	go func() {
-		wg.Wait()
-		close(supplierCh)
+		for res := range resCh {
+			suppliers := res.([]model.Supplier) // Cast `any` to `[]model.Supplier`
+			log.Printf("Received suppliers for a page, appending to result")
+			suppliersResult = append(suppliersResult, suppliers...)
+			log.Printf("Total number of suppliers fetched: %d", len(suppliersResult))
+		}
 	}()
 
-	// Listen for errors and results
-	select {
-	case err := <-errCh:
-		return err
-	case <-doneCh:
-		// Insert all fetched suppliers into the database
-		err := s.repo.InsertSuppliers(allSuppliers)
-		if err != nil {
-			return fmt.Errorf("error inserting suppliers into database: %v", err)
-		}
-		log.Println("Suppliers inserted into the database successfully!")
+	// Collect errors and close channels after all results are processed
+	go func() {
+		wPool.Shutdown() // This will close resCh and errCh
+		close(errCh)     // Close error channel after worker pool shuts down
+	}()
+
+	// Collect errors
+	var errors []error
+	for err := range errCh {
+		errors = append(errors, err)
 	}
+
+	// Check for errors
+	if len(errors) > 0 {
+		log.Printf("Errors occurred while fetching suppliers: %v", errors)
+		return fmt.Errorf("errors occurred while fetching suppliers: %v", errors)
+	}
+
+	// Verify the total number of suppliers fetched
+	log.Printf("Total number of suppliers fetched: %d", len(suppliersResult))
+
+	// Insert suppliers into the database
+	log.Println("Inserting suppliers into the database")
+	log.Printf("Attempting to insert %d suppliers into the database...", len(suppliersResult))
+
+	if err := s.repo.InsertSuppliers(suppliersResult); err != nil {
+		log.Printf("Failed to insert suppliers: %v", err)
+		return err
+	}
+
+	log.Println("Successfully finished fetching and inserting suppliers")
 
 	return nil
 }
