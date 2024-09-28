0a6aaa5b33942a3df0e5d15b5cca9b5b5cdba6e0
adhitiani.suzanthi <adhitiani@gmail.com>
2024-09-28 15:25:10 +0200

    modified cors middleware

--- util/util.go
+++ util/util.go
@@ -23,11 +23,14 @@ func CreateInsecureClient() *http.Client {
 // Middleware to handle CORS
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins; adjust as needed
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
+		// Handle preflight requests
 		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent) // Respond with 204 No Content
 			return
 		}
 
